6beb420bce43745cd3022b62a6c812e4af92c30b
ngdiaz02 <ngdiaz02@misena.edu.co>
2021-09-21 11:18:39 -0500

    1er caso de uso - Parcial

--- AsistenteHorariosInstructores/urls.py
+++ AsistenteHorariosInstructores/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from asistenteHorarios.FuncSprint1.CargarBDinicial import inBD
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/', inBD),
 ]
--- asistenteHorarios/FuncSprint1/CargarBDinicial.py
+++ asistenteHorarios/FuncSprint1/CargarBDinicial.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
-from asistenteHorarios import models
+from asistenteHorarios.models import Instructores, Contratacion, Ambientes
+import csv, io
+from django.contrib import messages
 
 # Esta función maneja la introducción inicial de datos a la BD
 
 def inBD(request):
-    #connection = MySQLdb.Connect(host='**', user='**', passwd='**', db='**')
-    cursor = connection.cursor()
-    query = "LOAD DATA INFILE '/path/to/my/file' INTO TABLE sometable FIELDS TERMINATED BY ';' ENCLOSED BY '\"' ESCAPED BY '\\\\'"
-    cursor.execute( query )
-    connection.commit()
-    return(request,"cargarBD.html")
+    template = "CargarBD.html"
+    data = Ambientes.objects.all()
+    prompt = {
+        'order': 'Order of the CSV should be name, email, address, phone, profile',
+        'profiles': data    
+              }
+    if request.method == "GET":
+        return render(request, template, prompt)
+    ctx = {}
+    csv_file = request.FILES['file']
+    if not csv_file.name.endswith('.csv'):
+        messages.error(request, 'THIS IS NOT A CSV FILE')
+    data_set = csv_file.read().decode('UTF-8')
+    io_string = io.StringIO(data_set)
+    next(io_string)
+    for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+        created = Ambientes.objects.update_or_create(
+        id_Ambiente = column[0],
+        Descripcion_Infraestructura = column[1],
+        Capacidad = column[2],
+        TipoDeAmbiente = column[3]
+        )
+        # created1 = Contratacion.objects.update_or_create(
+        # Fecha_Inicio = column[3],
+        # Fecha_Fin = column[4],
+        # Supervisora = column[5]
+        # )
+    
+    return render(request, template, ctx)
