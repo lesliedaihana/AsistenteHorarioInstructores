b7277074e1d12de448e6ca7a2836d8d9f85f9d65
lesliedaihana <leslieyayix@gmail.com>
2021-12-17 19:32:12 -0500

    avance daihana

---
+++ asistenteHorarios/testSena15Daihana.py
@@ -0,0 +1,19 @@
+
+from django.test import TestCase
+from asistenteHorarios.FuncSprint1.Sena15 import sena15Prueba
+
+
+
+
+
+class sena15(TestCase):
+   def testDA(self):
+       self.assertEquals (sena15.durEvento,5)
+       
+    
+
+
+
+
+
+
